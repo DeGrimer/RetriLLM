843a5da5ad1c5705c89f2b3cc46a5435bd823519
Danil Filippov <danil2609qwe@mail.ru>
2024-10-06 17:46:04 +0400

    Add web ui for q&a bot

--- QA_Bot.py
+++ QA_Bot.py
@@ -13,6 +13,8 @@ import torch
 from uuid import uuid4
 import json
 from pathlib import Path
+import os
+from dataclasses import dataclass
 
 # Define the metadata extraction function.
 def metadata_func(record: dict, metadata: dict) -> dict:
@@ -25,59 +27,23 @@ def metadata_func(record: dict, metadata: dict) -> dict:
         metadata["source"] = "\\".join(source)
 
     return metadata
+def load_docs(vector_store):
+    #Load data from json file
+    loader = JSONLoader(
+        file_path='./langchain/dataTK.json',
+        jq_schema='.[]',
+        content_key="content",
+        metadata_func=metadata_func)
 
-#Load data from json file
-loader = JSONLoader(
-    file_path='./langchain/dataTK.json',
-    jq_schema='.[]',
-    content_key="content",
-    metadata_func=metadata_func)
+    docs = loader.load()
 
-docs = loader.load()
+    #Split docs
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
 
-#Split docs
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
-
-#Embedding model for bi encoder
-embeddings = HuggingFaceEmbeddings(model_name="sergeyzh/rubert-tiny-turbo")
-
-#Create vector db
-vector_store = Chroma(
-    collection_name="example_collection",
-    embedding_function=embeddings,
-    persist_directory="./chroma_langchain_dbV3",  # Where to save data locally, remove if not necessary
-)
-
-uuids = [str(uuid4()) for _ in range(len(splits))]
-#Add docs to vector db
-vector_store.add_documents(documents=splits, ids=uuids)
-
-retriever = vector_store.as_retriever(
-    search_kwargs={"k": 15}
-)
-
-# Add cross-encoder for rerank docs from bi encoder
-model = HuggingFaceCrossEncoder(model_name='DiTy/cross-encoder-russian-msmarco')
-compressor = CrossEncoderReranker(model=model, top_n=5)
-compression_retriever = ContextualCompressionRetriever(
-    base_compressor=compressor, base_retriever=retriever
-)
-
-#Add LLM model
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype="float16",
-    bnb_4bit_use_double_quant=True,
-)
-model_name = "Vikhrmodels/Vikhr-Llama3.1-8B-Instruct-R-21-09-24"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=quantization_config)
-
-#Create pipeline for LLM model
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=256, top_k=50, temperature=0.1)
-llm = HuggingFacePipeline(pipeline=pipe)
+    uuids = [str(uuid4()) for _ in range(len(splits))]
+    #Add docs to vector db
+    vector_store.add_documents(documents=splits, ids=uuids)
 
 #Convert relevant docs to json format
 def format_docs(docs):
@@ -91,20 +57,64 @@ def format_docs(docs):
         list_doc.append(curDoc)
     return json.dumps(list_doc, ensure_ascii=False)
 
-#prompt
-template = """system: Your task is to answer the user's questions using only the information from the provided documents. Give two answers to each question: one with a list of relevant document identifiers and the second with the answer to the question itself, using documents with these identifiers.
- document: {context}
- user: {question}
- assistant: """
-custom_rag_prompt = PromptTemplate.from_template(template)
+@dataclass
+class BotConfig:
+    biencoder_model_name: str = "sergeyzh/rubert-tiny-turbo"
+    vector_db_directory: str = "./chroma_langchain_db"
+    k: int = 15 
+    cross_encoder_model_name: str = "DiTy/cross-encoder-russian-msmarco"
+    llm_model_name: str = "Vikhrmodels/Vikhr-Llama3.1-8B-Instruct-R-21-09-24"
 
-#create chain for app
-rag_chain = (
-    {"context": compression_retriever | format_docs, "question": RunnablePassthrough()}
-    | custom_rag_prompt
-    | llm
-    | StrOutputParser())
+class QABot():
+    def __init__(self, config : BotConfig):
+        #Embedding model for bi encoder
+        self.biencoder = HuggingFaceEmbeddings(model_name=config.biencoder_model_name)
+        #Load vector db
+        self.vector_store = Chroma(
+            collection_name="example_collection",
+            embedding_function=self.biencoder,
+            persist_directory=config.vector_db_directory,
+        )
+        if len(self.vector_store.get()['metadatas']) == 0:
+            load_docs(self.vector_store)
+        self.retriever = self.vector_store.as_retriever(
+            search_kwargs={"k": config.k}
+        )
 
-asi_msg = rag_chain.invoke("Могу ли я взять отпуск на время сессии при обучении в университете?")
-print(asi_msg.split("assistant: ")[-1])
+        # Add cross-encoder for rerank docs from bi encoder
+        self.cross_encoder = HuggingFaceCrossEncoder(model_name=config.cross_encoder_model_name)
+        self.compressor = CrossEncoderReranker(model=self.cross_encoder, top_n=5)
+        self.compression_retriever = ContextualCompressionRetriever(
+            base_compressor=self.compressor, base_retriever=self.retriever
+        )
+
+        #Add LLM model
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype="float16",
+            bnb_4bit_use_double_quant=True,
+        )
+        llm_model_name = config.llm_model_name
+        self.tokenizer = AutoTokenizer.from_pretrained(llm_model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(llm_model_name, device_map="auto", quantization_config=quantization_config)
+        pipe = pipeline("text-generation", model=self.model, tokenizer=self.tokenizer, max_new_tokens=512, top_k=50, temperature=0.1)
+        self.llm = HuggingFacePipeline(pipeline=pipe)
+
+        #prompt
+        template = """system: Your task is to answer the user's questions using only the information from the provided documents. Give two answers to each question: one with a list of relevant document identifiers and the second with the answer to the question itself, using documents with these identifiers.
+        document: {context}
+        user: {question}
+        assistant: """
+        self.custom_rag_prompt = PromptTemplate.from_template(template)
+
+        #create chain for app
+        self.rag_chain = (
+            {"context": self.compression_retriever | format_docs, "question": RunnablePassthrough()}
+            | self.custom_rag_prompt
+            | self.llm
+            | StrOutputParser())
+    def forward(self, input):
+        asi_msg = self.rag_chain.invoke(input)
+        return asi_msg.split("assistant: ")[-1]
 
---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from QA_Bot import BotConfig, QABot
+
+conf = BotConfig()
+bot = QABot(conf)
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!!!!"}
+
+class Query(BaseModel):
+    message: str
+
+@app.post("/chatbot/")
+async def get_response(query: Query):
+    print(query.message)
+    response = process_message(query.message)
+    return {"response": response}
+
+def process_message(message: str) -> str:
+    answer = bot.forward(message)
+    return answer
